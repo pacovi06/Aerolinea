3bcc4ce337cd4d7dc5576f2f254aae63635f2c20
marisolde <marisol_d_e@hotmail.com>
2015-05-23 03:22:04 -0500

    buscar1

--- buscarVuelo.php
+++ buscarVuelo.php
@@ -23,6 +23,17 @@
 			$user = "root";
 			$pw = "";
 			$db = "aerolinea";
+			
+			echo "<tr>";    
+						echo "<th>Identificador</th>";  
+						echo "<th>Origen</th>"; 
+						echo "<th>Destino</th>"; 
+						echo "<th>FechaSalida</th>"; 
+						echo "<th>FechaLlegada</th>"; 
+						echo "<th>HoraIda</th>"; 
+						echo "<th>HoraLlegada</th>"; 
+						echo "<th>Estado</th>"; 
+						echo "</tr>"; 
 	
 	$con = mysql_connect($host,$user,$pw)
 	or die ("Problemas al conectar server");
@@ -30,7 +41,7 @@
 	mysql_select_db($db, $con)
 	or die("problemas al conectar db");
 	
-	$registro = mysql_query("SELECT * FROM vuelos_disponibles WHERE Origen='$_POST[Origen]'AND Destino ='$_POST[Destino]'")
+	$registro = mysql_query("SELECT * FROM vuelos_disponibles WHERE Origen='$_POST[Origen]' AND Destino ='$_POST[Destino]' AND FechaIda='$_POST[ida]' AND FechaRegreso='$_POST[FechaRegreso]'")
 	
 	or die ("problemas en consulta:".mysql_error());
 	
@@ -38,7 +49,8 @@
 		
 		echo $reg['Origen']."<br>";
 		echo $reg['Destino']."<br>";
-		echo $reg['FechaSalida']."<br>";
+		echo $reg['FechaIda']."<br>";
+		echo $reg['FechaRegreso']."<br>";
 	}
 	
 	
